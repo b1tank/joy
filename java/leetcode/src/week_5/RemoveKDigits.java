8cef7bf5328fac95da8e86a20b76269c7ee767aa
b1tank <b1tank@outlook.com>
2023-02-25 23:20:08 -0800

    add a classic stack problem!

---
+++ java/leetcode/src/week_5/RemoveKDigits.java
@@ -0,0 +1,36 @@
+package week_5;
+
+import java.util.Stack;
+
+public class RemoveKDigits {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i=0; i<num.length(); i++) {
+            while (k > 0 && !st.empty() && st.peek() > num.charAt(i)) {
+                st.pop();
+                k--;
+            }
+
+            st.push(num.charAt(i));
+        }
+
+        while (k > 0) {
+            st.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!st.empty()) {
+            sb.append(st.pop());
+        }
+
+        while (sb.length() > 0 && sb.charAt(sb.length()-1) == '0') {
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+        sb.reverse();
+
+        return sb.isEmpty() ? "0" : sb.toString();
+    }
+}
